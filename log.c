d6667b4087935de52b260126c46a63c937514658
Felipe Contreras <felipe.contreras@nokia.com>
2009-06-03 18:29:47 +0300

    log: whitespace cleanup
    Signed-off-by: Felipe Contreras <felipe.contreras@nokia.com>

--- log.c
+++ log.c
@@ -36,13 +36,13 @@ const char *debug_id = "log";
 static inline const char *
 log_level_to_string(unsigned int level)
 {
-    switch (level)
-    {
-        case 0: return "error"; break;
-        case 1: return "warning"; break;
-        case 2: return "info"; break;
-        default: return NULL; break;
-    }
+	switch (level)
+	{
+		case 0: return "error"; break;
+		case 1: return "warning"; break;
+		case 2: return "info"; break;
+		default: return NULL; break;
+	}
 }
 
 void pr_helper(unsigned int level,
@@ -52,45 +52,45 @@ void pr_helper(unsigned int level,
 	       const char *fmt,
 	       ...)
 {
-    char *tmp;
-    va_list args;
+	char *tmp;
+	va_list args;
 
-    if (level < debug_level)
-	    return;
+	if (level < debug_level)
+		return;
 
-    va_start (args, fmt);
+	va_start(args, fmt);
 
-    vasprintf (&tmp, fmt, args);
+	vasprintf(&tmp, fmt, args);
 
 #ifdef EXTRA_DEBUG
-    fprintf(stderr, "%s %s:%d:%s() %s\n",
-	    log_level_to_string (level),
-	    file, line, function,
-	    tmp);
+	fprintf(stderr, "%s %s:%d:%s() %s\n",
+		log_level_to_string(level),
+		file, line, function,
+		tmp);
 #else
-    fprintf(stderr, "%s: %s\n",
-	    log_level_to_string (level),
-	    tmp);
+	fprintf(stderr, "%s: %s\n",
+		log_level_to_string(level),
+		tmp);
 #endif
 
 #ifdef SYSLOG
-    {
-	    int syslog_level;
-	    switch(level) {
-		    case 0:
-		    syslog_level = LOG_ERR;
-		    case 1:
-		    syslog_level = LOG_WARNING;
-		    case 2:
-		    syslog_level = LOG_INFO;
-		    default:
-		    syslog_level = LOG_DEBUG;
-	    }
-	    syslog(syslog_level, "%s", tmp);
-    }
+	{
+		int syslog_level;
+		switch(level) {
+			case 0:
+				syslog_level = LOG_ERR;
+			case 1:
+				syslog_level = LOG_WARNING;
+			case 2:
+				syslog_level = LOG_INFO;
+			default:
+				syslog_level = LOG_DEBUG;
+		}
+		syslog(syslog_level, "%s", tmp);
+	}
 #endif
 
-    free (tmp);
+	free(tmp);
 
-    va_end (args);
+	va_end(args);
 }
